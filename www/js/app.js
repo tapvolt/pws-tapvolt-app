1787e7dfcafd96b25639cbd8ed263d8380847d89
Gareth Jackson <gareth.jackson@gmail.com>
2015-04-30 22:04:46 +0100

    Chapter 11 complete

--- www/js/app.js
+++ www/js/app.js
@@ -9,17 +9,18 @@
     EmployeeListView.prototype.template = Handlebars.compile( $( "#employee-list-tpl" ).html() );
     EmployeeView.prototype.template = Handlebars.compile( $("#employee-tpl").html() );
     var service = new EmployeeService();
+    var slider = new PageSlider( $( "body" ) );
 
     service.initialize().done( function() {
         router.addRoute( "", function() {
-            $( "body" ).html( new HomeView( service ).render().$el );
+            slider.slidePage( new HomeView( service ).render().$el );
         } );
         console.log( "Service initialized" );
     } );
 
     router.addRoute( "employees/:id", function( id ) {
         service.findById( parseInt( id ) ).done( function( employee ) {
-            $( "body" ).html(new EmployeeView( employee ).render().$el );
+            slider.slidePage(new EmployeeView( employee ).render().$el );
         } );
     } );
 
