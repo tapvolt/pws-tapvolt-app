7e13725aea321a9aeb061e7b64fea191b9b63652
Gareth Jackson <gareth.jackson@gmail.com>
2015-04-30 21:58:34 +0100

    Chapter 10

--- www/js/app.js
+++ www/js/app.js
@@ -7,11 +7,23 @@
     /* ---------------------------------- Local Variables ---------------------------------- */
     HomeView.prototype.template = Handlebars.compile( $( "#home-tpl" ).html() );
     EmployeeListView.prototype.template = Handlebars.compile( $( "#employee-list-tpl" ).html() );
+    EmployeeView.prototype.template = Handlebars.compile( $("#employee-tpl").html() );
     var service = new EmployeeService();
+
     service.initialize().done( function() {
-        $( "body" ).html( new HomeView( service ).render().$el );
+        router.addRoute( "", function() {
+            $( "body" ).html( new HomeView( service ).render().$el );
+        } );
         console.log( "Service initialized" );
     } );
+
+    router.addRoute( "employees/:id", function( id ) {
+        service.findById( parseInt( id ) ).done( function( employee ) {
+            $( "body" ).html(new EmployeeView( employee ).render().$el );
+        } );
+    } );
+
+    router.start();
 
     /* --------------------------------- Event Registration -------------------------------- */
     // Override default HTML alert with native dialog
