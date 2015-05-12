4e6cfdbed21c351a08e1d6cb5e7bd22cee09efde
Gareth Jackson <gareth.jackson@gmail.com>
2015-05-12 23:04:49 +0100

    refactor

--- www/js/app.js
+++ www/js/app.js
@@ -1,36 +1,26 @@
-/**
- * Created by gjackson on 06/05/15.
- */
-( function() {
+"use strict";
 
-    "use strict";
+var myApp = angular.module( "myApp", [ "ngTouch", "ngRoute", "ngAnimate" ] )
 
-    angular.module( "myApp", [
-        "ngTouch",
-        "ngRoute",
-        "ngAnimate",
-        "myApp.controllers",
-        "myApp.memoryServices"
-    ] ).
-    config( [ "$routeProvider", function( $routeProvider ) {
-            $routeProvider.when(
-                "/observations", {
-                    templateUrl: "partials/observation-list.html",
-                    controller: "ObservationsListCtrl"
-                }
-            );
-            $routeProvider.when(
-                "/observations/:observationId", {
-                    templateUrl: "partials/observation-detail.html",
-                    controller: "ObservationDetailCtrl"
-                }
-            );
-            $routeProvider.when(
-                "/employees/:employeeId/reports", {
-                    templateUrl: "partials/report-list.html",
-                    controller: "ReportListCtrl"
-                }
-            );
-            $routeProvider.otherwise( { redirectTo: "observations" } );
-        } ] );
-} () );
+.config( [ "$routeProvider", function( $routeProvider ) {
+    $routeProvider.when(
+        "/observations", {
+            templateUrl: "partials/observation-list.html",
+            controller: "ObservationsListCtrl"
+        }
+    );
+    $routeProvider.when(
+        "/observations/:observationId", {
+            templateUrl: "partials/observation-detail.html",
+            controller: "ObservationDetailCtrl"
+        }
+    );
+    $routeProvider.when(
+        "/employees/:employeeId/reports", {
+            templateUrl: "partials/report-list.html",
+            controller: "ReportListCtrl"
+        }
+    );
+    $routeProvider.otherwise( { redirectTo: "observations" } );
+}])
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,25 +1,25 @@
 'use strict';
 
-angular.module( 'myApp.controllers', [] )
-    .controller( 'MainCtrl', ['$scope', '$rootScope', '$window', '$location', function($scope, $rootScope, $window, $location) {
-        $scope.slide = '';
-        $rootScope.back = function() {
-            $scope.slide = 'slide-right';
-            $window.history.back();
-        }
-        $rootScope.go = function(path){
-            $scope.slide = 'slide-left';
-            $location.url(path);
-        }
-    }])
-    .controller( "ObservationsListCtrl", [ "$scope", "$http", "Observation", function( $scope, $http, Observation ) {
-        $scope.observations = Observation.query();
-    } ] )
-    .controller( "ObservationDetailCtrl", [ "$scope", "$routeParams", "Observation", function( $scope, $routeParams, Observation ) {
-        $scope.observation = Observation.get( {
-                observationId: $routeParams.observationId
-            } );
-    } ] )
-    .controller('ReportListCtrl', ['$scope', '$routeParams', 'Report', function ($scope, $routeParams, Report) {
-        $scope.employees = Report.query({employeeId: $routeParams.employeeId});
-    }]);
+myApp
+.controller( 'MainCtrl', ['$scope', '$rootScope', '$window', '$location', function($scope, $rootScope, $window, $location) {
+    $scope.slide = '';
+    $rootScope.back = function() {
+        $scope.slide = 'slide-right';
+        $window.history.back();
+    }
+    $rootScope.go = function(path){
+        $scope.slide = 'slide-left';
+        $location.url(path);
+    }
+}])
+.controller( "ObservationsListCtrl", [ "$scope", "$http", "observationService", function( $scope, $http, observationService ) {
+    $scope.observations = observationService.query();
+} ] )
+.controller( "ObservationDetailCtrl", [ "$scope", "$routeParams", "Observation", function( $scope, $routeParams, Observation ) {
+    $scope.observation = Observation.get( {
+            observationId: $routeParams.observationId
+        } );
+} ] )
+.controller('ReportListCtrl', ['$scope', '$routeParams', 'Report', function ($scope, $routeParams, Report) {
+    $scope.employees = Report.query({employeeId: $routeParams.employeeId});
+}]);
